40fc9486109765e9b078d6812a8e41c5f3dc4577
fire332 <96039230+fire332@users.noreply.github.com>
2026-07-27 14:54:17 -0700

    feat: add `color-scheme` meta tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
 	import faviconIco from '$lib/assets/favicon.ico?no-inline';
 	import favicon from '$lib/assets/favicon.svg';
 	import favicon48 from '$lib/assets/favicon-48x48.png';
+	import { heroColorValues } from '$lib/tokens';
 
 	import type { LayoutProps } from './$types';
 
@@ -17,6 +18,7 @@
 	{#each links as link (link.attrs?.['href'])}
 		<link {...link.attrs ?? {}} />
 	{/each}
+	<meta name="theme-color" content={heroColorValues.background} />
 	<link rel="icon" type="image/svg+xml" sizes="any" href={favicon} />
 	<link rel="icon" type="image/png" sizes="48x48" href={favicon48} />
 	<link rel="icon" type="image/x-icon" sizes="16x16 32x32 48x48" href={faviconIco} />
